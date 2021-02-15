4032f5beb5653737630a9e73796ce5da11ba334d
sarahrichards36 <78057084+sarahrichards36@users.noreply.github.com>
2021-02-15 12:14:41 -0600

    Add files via upload

---
+++ Mpg.js
@@ -0,0 +1,7 @@
+ <script>
+     var floatMiles, floatGallons, floatMpg;
+    var floatMiles = prompt("Enter miles driven");
+     var floatGallons = prompt("Enter gallons of gas used");
+     var floatMpg = floatMiles/floatGallons;
+     alert("Miles per gallon = " + floatMpg);
+   </script>
